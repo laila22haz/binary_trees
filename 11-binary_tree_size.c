9b433a78e73c7a3fe603a4677e24080dbbe16e8e
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-08-29 14:08:14 -0700

    task

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,5 +8,4 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-}
 
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,5 +8,17 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-}
+	size_t count = 0;
 
+        if (tree == NULL)
+                return (0);
+
+        if (tree->left == NULL || tree->right == NULL)
+                count++;
+
+	if (tree->left)
+		count += binary_tree_nodes(tree->left);
+	if (tree->right)
+		count += binary_tree_nodes(tree->right);
+        return (count);
+}
