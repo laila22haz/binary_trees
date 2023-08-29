3bb1177f91e1dfaf9126b94ec2a89f2eaac8ea97
laila22haz <hazmirlaila316@gmail.com>
2023-08-29 12:41:04 +0100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,5 +8,27 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int count_right, count_left;
+	const binary_tree_t *origin_tree;
+
+	origin_tree = tree;
+	if (tree == NULL)
+		return (0);
+
+	count_left = 0;
+	while (tree->left != NULL)
+	{
+		tree = tree->left;
+		count_left++;
+	}
+
+	tree = origin_tree;
+	count_right = 0;
+	while (tree->right != NULL)
+	{
+		tree = tree->right;
+		count_right++;
+	}
+return (count_left > count_right ? count_left : count_right);
 }
 
