652cbadcfaf1506bee1af2f8913bcb2faa303e00
laila22haz <hazmirlaila316@gmail.com>
2023-08-30 17:56:24 +0100

    1-binary_tree_insert_left.c betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		tmp = parent->left;
-	       	parent->left = newNode;
+		parent->left = newNode;
 		tmp->parent = newNode;
 		newNode->left = tmp;
 	}
