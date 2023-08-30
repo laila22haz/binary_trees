77aa77289d0151e1947c5b02bf1933939feb2465
laila22haz <hazmirlaila316@gmail.com>
2023-08-30 18:17:02 +0100

    13-binary_tree_nodes.c betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,11 +10,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
-	if (tree == NULL)
-		return (0);
+		if (tree == NULL)
+			return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
-		count++;
+		if (tree->left != NULL || tree->right != NULL)
+			count++;
 
 	if (tree->left)
 		count += binary_tree_nodes(tree->left);
